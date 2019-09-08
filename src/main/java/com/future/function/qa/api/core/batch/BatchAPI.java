6f785bfa7e38495cf7843c50f7dfc954c932052e
Jonathan <jonathan16998@gmail.com>
2019-09-08 17:07:58 +0700

    Refactor API function related to cookie

--- src/main/java/com/future/function/qa/api/core/batch/BatchAPI.java
+++ src/main/java/com/future/function/qa/api/core/batch/BatchAPI.java
@@ -2,6 +2,8 @@ package com.future.function.qa.api.core.batch;
 
 import static com.future.function.qa.util.Path.BATCH;
 
+import java.util.function.Supplier;
+
 import com.future.function.qa.api.BaseAPI;
 import com.future.function.qa.model.request.core.batch.BatchWebRequest;
 import io.restassured.http.ContentType;
@@ -15,21 +17,20 @@ public class BatchAPI extends BaseAPI {
   @Step
   public Response create(BatchWebRequest request, Cookie cookie) {
 
-    return doByCookiePresent(cookie, () -> createBatchWithCookie(request, cookie),
-        () -> createBatchWithoutCookie(request));
+    return doByCookiePresent(cookie, createBatchWithCookie(request, cookie), createBatchWithoutCookie(request));
   }
 
-  private Response createBatchWithCookie(BatchWebRequest request, Cookie cookie) {
+  private Supplier<Response> createBatchWithCookie(BatchWebRequest request, Cookie cookie) {
 
-    return base.body(request)
+    return () -> base.body(request)
         .contentType(ContentType.JSON)
         .cookie(cookie)
         .post();
   }
 
-  private Response createBatchWithoutCookie(BatchWebRequest request) {
+  private Supplier<Response> createBatchWithoutCookie(BatchWebRequest request) {
 
-    return base.body(request)
+    return () -> base.body(request)
         .contentType(ContentType.JSON)
         .post();
   }
@@ -37,37 +38,37 @@ public class BatchAPI extends BaseAPI {
   @Step
   public Response delete(String id, Cookie cookie) {
 
-    return doByCookiePresent(cookie, () -> deleteBatchWithCookie(id, cookie), () -> deleteBatchWithoutCookie(id));
+    return doByCookiePresent(cookie, deleteBatchWithCookie(id, cookie), deleteBatchWithoutCookie(id));
   }
 
-  private Response deleteBatchWithCookie(String id, Cookie cookie) {
+  private Supplier<Response> deleteBatchWithCookie(String id, Cookie cookie) {
 
-    return base.cookie(cookie)
+    return () -> base.cookie(cookie)
         .delete(String.format(PATH_ID, id));
   }
 
-  private Response deleteBatchWithoutCookie(String id) {
+  private Supplier<Response> deleteBatchWithoutCookie(String id) {
 
-    return base.delete(String.format(PATH_ID, id));
+    return () -> base.delete(String.format(PATH_ID, id));
   }
 
   @Step
   public Response edit(BatchWebRequest request, Cookie cookie) {
 
-    return doByCookiePresent(cookie, () -> editBatchWithCookie(request, cookie), () -> editBatchWithoutCookie(request));
+    return doByCookiePresent(cookie, editBatchWithCookie(request, cookie), editBatchWithoutCookie(request));
   }
 
-  private Response editBatchWithCookie(BatchWebRequest request, Cookie cookie) {
+  private Supplier<Response> editBatchWithCookie(BatchWebRequest request, Cookie cookie) {
 
-    return base.body(request)
+    return () -> base.body(request)
         .contentType(ContentType.JSON)
         .cookie(cookie)
         .put(String.format(PATH_ID, request.getId()));
   }
 
-  private Response editBatchWithoutCookie(BatchWebRequest request) {
+  private Supplier<Response> editBatchWithoutCookie(BatchWebRequest request) {
 
-    return base.body(request)
+    return () -> base.body(request)
         .contentType(ContentType.JSON)
         .put(String.format(PATH_ID, request.getId()));
   }
@@ -75,20 +76,20 @@ public class BatchAPI extends BaseAPI {
   @Step
   public Response get(int page, int size, Cookie cookie) {
 
-    return doByCookiePresent(cookie, () -> getWithCookie(page, size, cookie), () -> getWithoutCookie(page, size));
+    return doByCookiePresent(cookie, getWithCookie(page, size, cookie), getWithoutCookie(page, size));
   }
 
-  private Response getWithCookie(int page, int size, Cookie cookie) {
+  private Supplier<Response> getWithCookie(int page, int size, Cookie cookie) {
 
-    return base.cookie(cookie)
+    return () -> base.cookie(cookie)
         .queryParam("page", page)
         .queryParam("size", size)
         .get();
   }
 
-  private Response getWithoutCookie(int page, int size) {
+  private Supplier<Response> getWithoutCookie(int page, int size) {
 
-    return base.queryParam("page", page)
+    return () -> base.queryParam("page", page)
         .queryParam("size", size)
         .get();
   }
@@ -96,18 +97,18 @@ public class BatchAPI extends BaseAPI {
   @Step
   public Response getDetail(String id, Cookie cookie) {
 
-    return doByCookiePresent(cookie, () -> getDetailWithCookie(id, cookie), () -> getDetailWithoutCookie(id));
+    return doByCookiePresent(cookie, getDetailWithCookie(id, cookie), getDetailWithoutCookie(id));
   }
 
-  private Response getDetailWithCookie(String id, Cookie cookie) {
+  private Supplier<Response> getDetailWithCookie(String id, Cookie cookie) {
 
-    return base.cookie(cookie)
+    return () -> base.cookie(cookie)
         .get(String.format(PATH_ID, id));
   }
 
-  private Response getDetailWithoutCookie(String id) {
+  private Supplier<Response> getDetailWithoutCookie(String id) {
 
-    return base.get(String.format(PATH_ID, id));
+    return () -> base.get(String.format(PATH_ID, id));
   }
 
   @Step
